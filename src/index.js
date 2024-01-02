b41a82f60f3b20e107f0e5a12e83c14eceb346f7
nishith <nishithmunda9212@gmail.com>
2024-01-02 21:49:06 +0530

    setup multer cloudinary cors mongo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB Connection Failed", err);
+    process.exit(1)
+  });
